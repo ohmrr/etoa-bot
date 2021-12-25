c992142d7c0cb3078f8b143db24b5a6b6bafcae4
Ohmrrr <89240639+Ohmrrr@users.noreply.github.com>
2021-12-24 23:57:49 -0800

    Finished avatar.js

--- src/Commands/avatar.js
+++ src/Commands/avatar.js
@@ -3,12 +3,22 @@ const Command = require('../Structures/Command.js');
 
 module.exports = new Command({
     name: 'avatar',
-    description: 'Sends a users\' avatar.',
+    description: "Sends a users' avatar.",
     usage: 'e!avatar [user]',
     userPermission: ['SEND_MESSAGES'],
-    botPermission: ['SEND_MESSAGES', 'ATTACH_FILES'],
+    botPermission: ['SEND_MESSAGES', 'EMBED_LINKS'],
 
     async execute(message, args, client) {
+        const member = message.mentions.members.first() || message.member;
+        const memberIcon = member.displayAvatarURL({ dynamic: true, format: 'png', size: 4096 });
 
+        const avatar = new MessageEmbed()
+            .setAuthor(
+                member.user.tag,
+                memberIcon
+            )
+            .setImage(memberIcon)
+        
+        message.channel.send({ embeds: [avatar] });
     },
 });
