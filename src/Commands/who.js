112850d3a0d92ca2b7f097aeaf67e98c4aae4a1e
Ohmrrr <89240639+Ohmrrr@users.noreply.github.com>
2021-12-29 23:42:48 -0800

    who.js, sends an embed of a random users profile picture in response to a question

---
+++ src/Commands/who.js
@@ -0,0 +1,32 @@
+const { MessageEmbed } = require('discord.js');
+const Command = require('../Structures/Command.js');
+
+module.exports = new Command({
+    name: 'who',
+    description: 'Replies to a question with a random user.',
+    usage: 'e!who <question>',
+    userPermission: ['SEND_MESSAGES'],
+    botPermission: ['SEND_MESSAGES', 'EMBED_LINKS'],
+
+    async execute(message, args, client) {
+        const question = args.splice(1).join(' ');
+        const whoResponse = new MessageEmbed()
+
+        if (!question) {
+            whoResponse
+                .setColor('RED')
+                .setDescription('What was your question? 🔮');
+            
+            return message.channel.send({ embeds: [whoResponse] });
+        }
+
+        const randMember = message.guild.members.cache.random();
+
+        whoResponse
+            .setColor('DARK_BUT_NOT_BLACK')
+            .setTitle(randMember.user.tag)
+            .setImage(randMember.displayAvatarURL({ dynamic: true, format: 'png', size: 4096 }));
+        
+        message.channel.send({ content: `🔮 ${message.author.username} 🔮`, embeds: [whoResponse] });
+    },
+});
