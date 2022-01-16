e3f796b0a5b8f4bc0dabb4111c5db98737611994
Ohmrrr <89240639+Ohmrrr@users.noreply.github.com>
2022-01-15 23:14:27 -0800

    one less newline in ready.js

--- src/Events/ready.js
+++ src/Events/ready.js
@@ -3,7 +3,7 @@ const Event = require('../Structures/Event.js');
 module.exports = new Event('ready', (client) => {
   client.logger(
     'ready',
-    `\n\nLogging in as ${client.user.tag}... \neTOA-001 is ready for use.\n`
+    `\nLogging in as ${client.user.tag}... \neTOA-001 is ready for use.\n`
   );
 
   client.user.setActivity('eTOA-001 | ' + client.prefix, { type: 'WATCHING' });
